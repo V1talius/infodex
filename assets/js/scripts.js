da9b97dbb2ef0bb1ec8c6dc5cfd7adf55862c331
shinTM <emasugeta@gmail.com>
2019-03-18 22:18:36 +0200

    wip

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,5 +1,7 @@
 jQuery( document ).ready( function( $ ) {
 
+	'use strict';
+
 	$( '#main-slider' ).sliderPro( {
 		width: '100%',
 		height: 770,
@@ -19,15 +21,72 @@ jQuery( document ).ready( function( $ ) {
 		}
 	} );
 
-	var swiper = new Swiper('.swiper-container', {
-		slidesPerView: 1,
+
+	var testiCarousel1 = new Swiper( '.testimonials-1', {
+		slidesPerView: 2,
 		spaceBetween: 30,
 		pagination: {
-			el: '.swiper-pagination',
+			el: '.testimonials-pagination-1',
 			clickable: true,
 		},
 	});
 
+	var testiCarousel2 = new Swiper( '.testimonials-2', {
+		slidesPerView: 1,
+		spaceBetween: 30,
+		pagination: {
+			el: '.testimonials-pagination-2',
+			clickable: true,
+		},
+	});
+
+	var brandsCarousel = new Swiper( '.brands', {
+		slidesPerView: 6,
+		spaceBetween: 30,
+		pagination: {
+			el: '.brands-pagination',
+			clickable: true,
+		},
+	});
+
+	$( '.section-10 h2' ).on( 'click', function(){
+		testiCarousel1.slideNext( 1000, true );
+	});
+
+	//////////////////fdkjhfkjdfg///////////////////
+
+	$( '.ellipse-avatar-button' ).on( 'click', function(){
+		randomize();
+	});
+
+	function randomize() {
+		var $ellipse__avatar        = $( '.ellipse__avatar' ),
+			$ellipse__avatar__item  = $( '.ellipse__avatar__item', $ellipse__avatar );
+
+			$ellipse__avatar__item.each( function( index ) {
+				var $item       = $( this ),
+					randomSide  = Math.round( Math.random() ),
+					randomXRage = randomSide == 0 ? { min: 0, max: 20 } : { min: 80, max: 100 },
+					randomX     = getRndInteger( randomXRage.min, randomXRage.max ),
+					randomY     = getRndInteger( 40, 60 ),
+					randomR     = Math.floor(Math.random() * 360);
+
+				$item.css( {
+					'top': randomY + '%',
+					'left': randomX + '%',
+					'transform': 'rotate(' + randomR + 'deg)'
+				});
+			});
+
+			console.log('--------------------');
+	}
+
+	randomize();
+
+	function getRndInteger(min, max) {
+		return Math.floor(Math.random() * (max - min) ) + min;
+	}
+	////////////////////////////////
 
 	var $tabs         = $( '.tabs' ),
 		$controlItems = $tabs.find( '.tabs__control-item'),
